6de7eea83413eb711fca6d5aca21f2f233352ee1
canger <canger@192.168.5.31>
2026-09-08 22:30:29 +0800

    fix: export GITHUB_URL from constant/env

--- web/src/constant/env.ts
+++ web/src/constant/env.ts
@@ -1,3 +1,5 @@
 export const APP_VERSION = __APP_VERSION__ || "dev";
 
 export const DOCS_URL = import.meta.env.VITE_DOC_URL || "https://api.772.ee";
+
+export const GITHUB_URL = "https://github.com/cangerx/c-aihuabu";
